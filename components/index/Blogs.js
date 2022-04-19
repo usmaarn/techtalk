2963775fdb5cd91c07e8928f84572124b32ebd5f
usmaarn <usmaarn@gmail.com>
2022-04-19 08:07:49 +0100

    added i dont know

--- components/index/Blogs.js
+++ components/index/Blogs.js
@@ -1,9 +1,8 @@
-import {images as blogs} from "@/components/index/Heading";
 import {RiTimeLine} from "react-icons/ri";
 import CustomLink from "@/components/CustomLink";
 import Image from "next/image";
 
-export default function Blogs(){
+export default function Blogs({blogs}){
     return(
         <div className='px-5 lg:px-32 mt-10 lg:mt-0 mb-20'>
             <h3 className='text-xl flex items-center gap-2 uppercase tracking-widest font-bold
@@ -13,22 +12,22 @@ export default function Blogs(){
 
             <div className="grid md:grid-cols-3 gap-10">
                 {blogs.map(blog => (
-                    <CustomLink href="#" key={blog.tag} className='block'>
+                    <CustomLink href="#" key={blog.id} className='block'>
                         <div className="border rounded-lg overflow-hidden cursor-pointer">
-                            <div className='bg-black hover:scale-110 transition duration-500'>
-                                <Image width={100}  height={100} src={`/images/techtalk/${blog.bgImage}`}
+                            <div className='bg-black h-36 overflow-hidden hover:scale-110 transition duration-500'>
+                                <Image width={400} height={300} objectFit="cover" src={`/images/techtalk/${blog.bgImage}`}
                                        className='opacity-50' alt={blog.title} />
                             </div>
                             <div className="py-5 font-medium">
-                                <span className='text-sm bg-blue-200 py-2 px-5'>{blog.tag}</span>
-                                <h5 className='mt-5 px-5 text-lg hover:underline leading-tight'>{blog.text}</h5>
+                                {/*<span className='text-sm bg-blue-200 py-2 px-5'>{blog.tag}</span>*/}
+                                <h5 className='px-5 text-lg hover:underline leading-tight'>{blog.title}</h5>
                             </div>
                         </div>
                     </CustomLink>
                 ))}
             </div>
             <CustomLink href="/blogs" className="float-right mt-5 hover:underline cursor-pointer font-bold text-blue-600">
-                Read More &gt;&gt;
+                Read More
             </CustomLink>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,23 @@ import WhatWeDo from "@/components/index/WhatWeDo";
 import Blogs from "@/components/index/Blogs";
 import NewsLetter from "@/components/index/NewsLetter";
 import Main from "@/components/layouts/Main";
+import Post from "../config/models/Post";
 
-export default function index(){
+export default function index({blogs}){
+
+    blogs = JSON.parse(blogs)
+
   return(
     <Main title="Home Page">
         <Heading />
         <WhatWeDo />
-        <Blogs />
+        <Blogs blogs={blogs} />
         <NewsLetter />
     </Main>
   )
+}
+
+export async function getServerSideProps(context){
+    const blogs = await Post.findAll();
+    return {props: {blogs: JSON.stringify(blogs)}}
 }
